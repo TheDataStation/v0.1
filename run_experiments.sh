47839361ea0a4d9ca0a58aae4e0252c3ad8a297b
Christopher Zhu <chz@uchicago.edu>
2024-10-28 15:08:05 -0500

    echo

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,5 +1,6 @@
 # for size in 1MB 10MB 100MB 1GB 10GB
 for size in 1MB 10MB
 do
+    echo "Running join_scenario_experiment for $size"
     python -m integration_new.join_scenario_experiment $size &> join_scenario_experiment_$size.log
 done
