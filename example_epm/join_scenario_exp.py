0a403ad390e332212edeb37d48f07a6e75b46873
Chris Zhu <chz@uchicago.edu>
2024-11-19 06:06:41 +0000

    remove sklearn

--- example_epm/join_scenario_exp.py
+++ example_epm/join_scenario_exp.py
@@ -3,8 +3,6 @@ from escrowapi.escrow_api import EscrowAPI
 
 import time
 import duckdb
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
 
 @api_endpoint
 def upload_data_in_csv(de_in_bytes):
@@ -48,48 +46,3 @@ def run_query(query):
     print("total query time: ", query_end_time - query_start_time)
     return res_df
 
-
-@api_endpoint
-@function
-def train_model_over_joined_data_v1(model_name, label_name, query=None):
-    # First check if the joined df has been preserved already
-    joined_de_id = EscrowAPI.load("joined_de_id")
-    res_df = None
-    if joined_de_id:
-        print(joined_de_id)
-        res_df = EscrowAPI.ObjectDEStore.read(joined_de_id)
-    elif query:
-        print("Need to preserve intermediate DEs!")
-        res_df = run_query(query)
-        joined_de_id = EscrowAPI.ObjectDEStore.write(res_df)
-        EscrowAPI.store("joined_de_id", joined_de_id["de_id"])
-    get_combined_data_time = time.time()
-    if res_df is not None:
-        X = res_df.drop(label_name, axis=1)
-        y = res_df[label_name]
-        if model_name == "logistic_regression":
-            # print("Training LOGIT model!!!")
-            clf = LogisticRegression().fit(X, y)
-            return clf.coef_, get_combined_data_time
-        elif model_name == "MLP":
-            clf = MLPClassifier()
-            clf.fit(X, y)
-            return clf.coefs_, get_combined_data_time
-
-
-@api_endpoint
-@function
-def train_model_over_joined_data_v2(model_name, label_name, query):
-    res_df = run_query(query)
-    # print("Result Dataframe is", res_df)
-    get_combined_data_time = time.time()
-    if res_df is not None:
-        X = res_df.drop(label_name, axis=1)
-        y = res_df[label_name]
-        if model_name == "logistic_regression":
-            clf = LogisticRegression().fit(X, y)
-            return clf.coef_, get_combined_data_time
-        elif model_name == "MLP":
-            clf = MLPClassifier()
-            clf.fit(X, y)
-            return clf.coefs_, get_combined_data_time
